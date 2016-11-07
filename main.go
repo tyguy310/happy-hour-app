d31540e73096a439364ed39a2d96e3702464de01
tyguy310 <tyler_maier@yahoo.com>
2016-11-07 13:08:25 -0700

    test db and test data

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    // "fmt"
+    "html/template"
+    "log"
+    "net/http"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+    "encoding/json"
+)
+
+type Page struct {
+  Name string
+  DBStatus bool
+}
+
+type SearchResult struct {
+  Title string
+  Author string
+  Year string
+  ID string
+}
+
+func main() {
+  templates := template.Must(template.ParseFiles("templates/index.html"))
+
+  db, _ :=sql.Open("sqlite3", "dev.db")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+      p := Page{Name: "Gopher"}
+      if name := r.FormValue("name"); name != "" {
+        p.Name = name
+      }
+      p.DBStatus = db.Ping() == nil
+
+      if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
+    })
+
+    http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+      results := []SearchResult{
+        SearchResult{"Moby-Dick", "Herman Melville", "1851", "2222"},
+      }
+      encoder := json.NewEncoder(w)
+      if err := encoder.Encode(results); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
